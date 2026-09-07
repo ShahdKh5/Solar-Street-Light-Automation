0079d15c2252dd86822a28fa8e1436ac8315cdac
Shahd Khalil <114525646+ShahdKh5@users.noreply.github.com>
2026-09-07 12:29:41 +0300

    Delete 03-Application directory

--- 03-Application/01-APP_WaterControl/APP_WaterControl_Config.h
+++
@@ -1,8 +0,0 @@
-#ifndef APP_WATERCONTROL_CONFIG_H
-#define APP_WATERCONTROL_CONFIG_H
-
-/* TODO: Define hysteresis thresholds */
-#define WATER_LEVEL_LOW      20  
-#define WATER_LEVEL_HIGH     80  
-
-#endif
